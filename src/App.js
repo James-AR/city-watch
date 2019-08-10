1fe2a3aeb621a3668a143057edfd1d1230142aeb
James-AR <ruiz.james12@yahoo.com>
2019-08-09 22:10:43 -0700

    Context set up

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import { Route, Switch, Link } from 'react-router-dom'
 
 import LandingPage from './pages/LandingPage'
 import HomePage from './pages/HomePage'
+import UserState from './context/userState'
 
 function App() {
   firestore.collection('users').doc('dsfsdf43ser32').delete()
 
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={LandingPage} />
-        <Route path='/homepage' component={HomePage} />
-      </Switch>
+      <UserState>
+        <Switch>
+          <Route exact path='/' component={LandingPage} />
+          <Route path='/homepage' component={HomePage} />
+        </Switch>
+      </UserState>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../context/userContext'
 
 const HomePage = () => {
+    const userContext = useContext(UserContext)
+    console.log(userContext.userState)
     return (
         <div>
             <h1>Home Page</h1>
