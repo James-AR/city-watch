603ffeedc7f21a7ff8625769208c9d11b44dd997
James-AR <ruiz.james12@yahoo.com>
2019-08-09 21:49:46 -0700

    Firebase hooked up with react router installation

---
+++ src/firebase/firebaseUtils.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyCgoDI-dtPMV5nkLKz7Uvkgrmp9fH_fa-s",
+    authDomain: "city-watch-c9fac.firebaseapp.com",
+    databaseURL: "https://city-watch-c9fac.firebaseio.com",
+    projectId: "city-watch-c9fac",
+    storageBucket: "",
+    messagingSenderId: "214907585095",
+    appId: "1:214907585095:web:eaf0aaaf107b10f8"
+  };
+
+firebase.initializeApp(config)
+
+export const firestore = firebase.firestore()
+
+export default firebase;
