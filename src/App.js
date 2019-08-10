8e6871bd68a7e72c9d4107582034ea8f38bf7f66
James-AR <ruiz.james12@yahoo.com>
2019-08-09 21:52:54 -0700

    Routing with Landing page and Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import { firestore } from './firebase/firebaseUtils'
+import { Route, Switch, Link } from 'react-router-dom'
+
+import LandingPage from './pages/LandingPage'
+import HomePage from './pages/HomePage'
 
 function App() {
+  firestore.collection('users').doc('dsfsdf43ser32').delete()
+
   return (
     <div>
-      <h1>React is up</h1>
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route path='/homepage' component={HomePage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
